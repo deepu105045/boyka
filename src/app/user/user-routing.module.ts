64241a7436c96e68a43d65d2d12d192e27915bc3
deepu105045 <deepu105045@gmail.com>
2018-12-02 21:41:59 +0530

    remove typo

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';\
+import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
